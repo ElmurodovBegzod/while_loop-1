a2ddbbfc6d2442afa1c2a94ebdd561154eb91cd7
Begzod <ebegzod477@gmail.com>
2022-08-10 22:32:15 +0500

    code08

--- bin/while_loop08.dart
+++ bin/while_loop08.dart
@@ -4,4 +4,19 @@
   
   
 */
-void main() {}
+String func(int a) {
+  int b = 0;
+  String c = "";
+  while (b < a) {
+    b++;
+    if (b % 2 == 1) {
+      c += b.toString();
+      if (b % 2 == 0) {}
+    }
+  }
+  return c;
+}
+
+void main() {
+  print(func(10));
+}
